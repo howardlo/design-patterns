32ae94cda9eae4d1c3893f8e46d645f6526c439d
Howard Lo <howardlo@outlook.com>
2017-08-11 11:57:52 -0400

    Check in DynamicVisitorX (C# 6.0) and VisitorDesignPatternX (regular static Visitor in C# from codeproject)

---
+++ DynamicVisitorX/DynamicVisitorX/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DynamicVisitorX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dynamic Visitor Pattern");
+
+            var person = new Person
+            {
+                FirstName = "Albert",
+                LastName = "Einstein",
+            };
+
+            person.Friends.Add(
+                new Employee
+                {
+                    FirstName = "Bill",
+                    LastName = "Gates",
+                    Salary = 5000
+                });
+
+            person.Friends.Add(
+                new Customer
+                {
+                    FirstName = "Sally",
+                    LastName = "Rides",
+                    CreditLimit = 1
+                });
+
+            person.Friends.Add(
+                new Employee
+                {
+                    FirstName = "Melinda",
+                    LastName = "Gates",
+                    Salary = 5000
+                });
+
+            Console.WriteLine(new ToXElementPersonVisitor().DynamicVisit(person));
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public readonly IList<Person> Friends = new Collection<Person>();
+    }
+
+    class Customer : Person
+    {
+        public decimal CreditLimit { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public decimal Salary { get; set; }
+    }
+
+    class ToXElementPersonVisitor
+    {
+        public XElement DynamicVisit(Person p) => Visit((dynamic)p);
+
+        XElement Visit(Person p)
+        {
+            return new XElement("Person",
+                new XAttribute("Type", p.GetType().Name),
+                new XAttribute("FirstName", p.FirstName),
+                new XAttribute("LastName", p.LastName),
+            p.Friends.Select(f => DynamicVisit(f)));
+        }
+
+        XElement Visit(Customer c) // Specialized logic for customers
+        {
+            XElement xe = Visit((Person)c); // Call "base" method
+            xe.Add(new XElement("CreditLimit", c.CreditLimit));
+            return xe;
+        }
+
+        XElement Visit(Employee e) // Specialized logic employees
+        {
+            XElement xe = Visit((Person)e); // Call "Base" method
+            xe.Add(new XElement("Salary", e.Salary));
+            return xe;
+        }
+    }
+}
---
+++ VisitorDesignPatternX/VisitorDesignPatternX/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//
+// From: https://www.codeproject.com/articles/186185/visitor-design-pattern
+//
+
+namespace VisitorDesignPatternX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first set up the structure
+            List<IElement> list = new List<IElement> ();
+            list.Add(new Household("The Adams Family"));
+            list.Add(new Household("The Jones Family"));
+            list.Add(new BusinessEntity("The Oatmeal Bakery"));
+            list.Add(new BusinessEntity("The Ice Cream Shop"));
+
+            //use one visitor, or logic
+            IVisitor visitor = new SantaClaus();
+            foreach (IElement i in list)
+                i.Accept(visitor);   //apply the logic to the element
+
+            //use another visitor, or logic
+            visitor = new MailCarrier();
+            foreach (IElement i in list)
+                i.Accept(visitor);  //apply the logic to the element
+
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+
+    public interface IElement
+    {
+        string Name { get; set; }
+        void Accept(IVisitor v);
+    }
+
+    public class Household : IElement
+    {
+        private string name;
+
+        string IElement.Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Household(string name)
+        {
+            this.name = name;
+        }
+
+        void IElement.Accept(IVisitor v)
+        {
+            v.Visit(this);
+        }
+    }
+
+    public class BusinessEntity : IElement
+    {
+        private string name;
+
+        string IElement.Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public BusinessEntity(string name)
+        {
+            this.name = name;
+        }
+
+        void IElement.Accept(IVisitor v)
+        {
+            v.Visit(this);
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(IElement e);
+    }
+
+    public class SantaClaus : IVisitor
+    {
+        void IVisitor.Visit(IElement v)
+        {
+            Console.WriteLine("Santa visited " + v.Name);
+        }
+    }
+
+    public class MailCarrier : IVisitor
+    {
+        void IVisitor.Visit(IElement v)
+        {
+            Console.WriteLine("MailCarrier visited " + v.Name);
+        }
+    }
+}
